0e393192831f2dc78dd80cb164e2b4834ba6d05a
Francis Chong <francis@ignition.hk>
2012-05-10 17:47:07 +0800

    add a static library for hpple

---
+++ HppleLib/HppleLib.m
@@ -0,0 +1,13 @@
+//
+//  HppleLib.m
+//  HppleLib
+//
+//  Created by Francis Chong on 12年5月10日.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HppleLib.h"
+
+@implementation HppleLib
+
+@end
